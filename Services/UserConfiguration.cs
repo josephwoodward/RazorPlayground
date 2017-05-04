e966dbaf3df1d7d3672825fe04a6ab35e4f4f00b
Joseph Woodward <joseph.woodward@xeuse.com>
2017-05-04 01:10:08 +0100

    Added stuff

---
+++ Services/UserConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace RazorDeepDive.Services
+{
+    public interface IUserConfiguration
+    {
+        string Name { get; }
+    }
+
+    public class UserConfiguration : IUserConfiguration
+    {
+        public string Name => "Gilfoyle";
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using RazorDeepDive.Services;
 
 namespace RazorDeepDive
 {
@@ -25,6 +26,7 @@ namespace RazorDeepDive
         {
             // Add framework services.
             services.AddMvc();
+            services.AddTransient<IUserConfiguration, UserConfiguration>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
