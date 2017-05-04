76f0383876cc2b35da72d289d5fcad4976101827
Joseph Woodward <joseph.woodward@xeuse.com>
2017-05-04 23:47:34 +0100

    Added condition

--- Services/UserConfiguration.cs
+++ Services/UserConfiguration.cs
@@ -3,10 +3,14 @@
     public interface IUserConfiguration
     {
         string Name { get; }
+
+        bool LoggedIn { get; }
     }
 
     public class UserConfiguration : IUserConfiguration
     {
         public string Name => "Gilfoyle";
+
+        public bool LoggedIn => true;
     }
 }
---
+++ TagHelpers/ConditionTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace RazorDeepDive.TagHelpers
+{
+    // Target any HTML element with condition attribute
+    [HtmlTargetElement(Attributes = nameof(Condition))]
+    public class ConditionTagHelper : TagHelper
+    {
+        public bool Condition { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!Condition)
+            {
+                output.SuppressOutput();
+            }
+        }
+    }
+}
