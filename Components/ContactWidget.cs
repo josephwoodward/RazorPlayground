f96b69d6928efde99100f4c8431ffedfcd52e529
Joseph Woodward <joseph.woodward@xeuse.com>
2017-05-05 00:55:52 +0100

    aded component

---
+++ Components/ContactWidget.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RazorDeepDive.Components
+{
+    public class ContactWidget : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
